136c191ac1dcab4b268f63f720808d05ed7344bb
slee <yelliteen@gmail.com>
2025-11-29 15:57:56 +0900

    chore: backend init

---
+++ backend/src/main/java/com/dokdok/DokdokApplication.java
@@ -0,0 +1,13 @@
+package com.dokdok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokdokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokdokApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dokdok/DokdokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokdok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokdokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
